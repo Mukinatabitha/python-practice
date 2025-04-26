55c99b61cb2bfecb9515460770225eb1319007d2
Mukinatabitha <mukinatabitha@gmail.com>
2025-04-26 19:05:43 +0300

    Add files via upload

---
+++ Listsum.py
@@ -0,0 +1,8 @@
+def sum_list(numbers):
+    total = 0  # Start with 0
+    for num in numbers:  # Go through each number in the list
+        total = total + num  # Add the number to total
+    return total  # Give back the final total
+
+# Try it out
+print(sum_list([1, 2, 3, 4]))  # Should print 10
